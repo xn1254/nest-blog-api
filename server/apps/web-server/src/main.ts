b5b508d3abdb43f10ce05fca6d3f05bad0c8cbb9
LAPTOP-U2BLCUTE\今天又被熊妹咬了 <781820862@qq.com>
2021-09-14 17:52:47 +0800

    文件暂存

--- server/apps/web-server/src/main.ts
+++ server/apps/web-server/src/main.ts
@@ -1,8 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2021-09-14 15:50:50
+ * @LastEditTime: 2021-09-14 17:49:02
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \个人博客\server\apps\web-server\src\main.ts
+ */
 import { NestFactory } from '@nestjs/core';
 import { WebServerModule } from './web-server.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(WebServerModule);
-  await app.listen(3000);
+  await app.listen(3001);
 }
 bootstrap();
